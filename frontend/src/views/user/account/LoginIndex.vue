28cd89cd25b1ec8432e683bdd01ec7687acffe67
Aludel <wjiantao762@gmail.com>
2026-09-25 15:08:02 +0800

    登录与注册界面的模块增加

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-  <div>登录</div>
+  <div class="flex justify-center mt-35">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+      <label class="label">邮箱</label>
+      <input type="email" class="input" placeholder="邮箱" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input" placeholder="密码" />
+
+      <label class="label">验证码</label>
+      <input type="password" class="input" placeholder="验证码" />
+
+      <button class="btn btn-neutral mt-4">登录</button>
+
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'user-account-register-index'}" class="btn btn-small btn-ghost text-gray-300">
+          注册
+        </RouterLink>
+      </div>
+
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -3,7 +3,31 @@
 </script>
 
 <template>
-  <div>注册</div>
+  <div class="flex justify-center mt-35">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+      <label class="label">邮箱</label>
+      <input type="email" class="input" placeholder="邮箱" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input" placeholder="密码" />
+
+      <label class="label">密码确认</label>
+      <input type="password" class="input" placeholder="密码确认" />
+
+      <label class="label">验证码</label>
+      <input type="password" class="input" placeholder="验证码" />
+
+      <button class="btn btn-neutral mt-4">注册</button>
+
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'user-account-login-index'}" class="btn btn-small btn-ghost text-gray-300">
+          登录
+        </RouterLink>
+      </div>
+
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
